d8a07090f07ee47405c3b3a3eff9a7d47e8f0063
Ann Thomas <ann-thomas2010@hotmail.com>
2025-12-26 13:36:00 -0500

    Add root API health check

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Project
 from .serializers import ProjectSerializer
+from django.http import HttpResponse
 
 
 @api_view(['GET'])
@@ -10,3 +11,7 @@ def project_list(request):
     serializer = ProjectSerializer(projects, many=True)
 
     return Response(serializer.data)
+
+
+def home(request):
+    return HttpResponse("Ann Thomas API is running")
